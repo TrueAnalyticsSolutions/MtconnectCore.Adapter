0ffc760bbd54ce7e3e1896793bb464e3acd5835f
Trais McAllister <tbm0115@hotmail.com>
2023-02-06 13:28:36 -0800

    Universal Service Installation (#14)
    * Updating BackgroundService
    
    Updating the publish of the Universal Service to be installable
    
    * Extra logging and Adapter error handling
    
     - Added error handling when processing new DataItems
     - Added Serilog to Service
     - Added lots of logging to Service and AdapterInterface
     - Added `reinstall.bat` to more easily update an existing installation

---
+++ Service/install.bat
@@ -0,0 +1,7 @@
+﻿@ECHO OFF
+
+echo Installing MTConnect Adapter Windows Service...
+sc create MTConnectAdapter displayName="MTConnect Adapter" BinPath="%~dp0Service.exe" Start=Auto
+sc description MTConnectAdapter "A MTConnect Adapter to be consumed by a MTConnect Agent"
+echo Done. Press enter to exit.
+pause
