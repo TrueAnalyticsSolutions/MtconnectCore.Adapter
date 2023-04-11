6341ff3717cfdf7ae883d98489fd844d32c6075e
Trais McAllister <tbm0115@hotmail.com>
2023-04-10 18:19:22 -0700

    Added property path

--- AdapterInterface/AdapterExtensions.cs
+++ AdapterInterface/AdapterExtensions.cs
@@ -75,15 +75,15 @@ namespace Mtconnect
                 typeof(Message),
                 typeof(TimeSeries)
         };
-        private static bool TryAddDataItems(this Adapter adapter, object model, string typePath = "", string dataItemNamePrefix = "", string dataItemDescriptionPrefix = "")
+        private static bool TryAddDataItems(this Adapter adapter, object model, string modelPath = "", string dataItemNamePrefix = "", string dataItemDescriptionPrefix = "")
         {
             bool isCached = true;
             bool allDataItemsAdded = true;
             lock (_dataItemProperties)
             {
                 Type dataModelType = model.GetType();
-                if (string.IsNullOrEmpty(typePath))
-                    typePath = dataModelType.FullName;
+                if (string.IsNullOrEmpty(modelPath))
+                    modelPath = dataModelType.FullName;
 
                 Dictionary<string, PropertyInfo> timestampPropertyLookup;
                 // Try to get the PropertyInfo for the DataItem Timestamp override, if it exists
@@ -146,7 +146,7 @@ namespace Mtconnect
                             switch (dataItemAttribute)
                             {
                                 case DataItemPartialAttribute _:
-                                    dataItemAdded = adapter.TryAddDataItems(property.GetValue(model), $"{typePath}[{property.Name}]", dataItemName, dataItemDescription);
+                                    dataItemAdded = adapter.TryAddDataItems(property.GetValue(model), $"{modelPath}[{property.Name}]", dataItemName, dataItemDescription);
                                     break;
                                 case EventAttribute _:
                                     dataItem = new Event(dataItemName, dataItemType, dataItemSubType, dataItemDescription);
@@ -201,7 +201,7 @@ namespace Mtconnect
                         // Now add the DataItem if it was constructed
                         if (dataItem != null)
                         {
-                            dataItem.ModelPath = typePath;
+                            dataItem.ModelPath = $"{modelPath}[{property.Name}]";
                             if (timestampProperty != null)
                             {
                                 dataItem.HasTimestampOverride = true;
